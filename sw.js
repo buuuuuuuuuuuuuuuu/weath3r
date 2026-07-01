40457ae045349ab379739dcf25eaaff905e7b9ae
buuuuuuuuuuuuuuuu <53521790+buuuuuuuuuuuuuuuu@users.noreply.github.com>
2026-07-01 08:37:38 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    - offline shell + last-data cache
    - immediate activation, then notifies open clients of updates
    ← bump CACHE on every deploy (matches APP_VERSION) */
-const CACHE = "weath3r-v7.6";
+const CACHE = "weath3r-v8.0";
 const ASSETS = [
   "./",
   "./index.html",
